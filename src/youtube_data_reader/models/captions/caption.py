6c5cb15782186fa088dd856744868371def93183
xpenalosa <xavier.penalosa.esteller@gmail.com>
2020-10-30 02:25:11 +0100

    Leftover Optional type

--- src/youtube_data_reader/models/captions/caption.py
+++ src/youtube_data_reader/models/captions/caption.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from dataclasses_json import dataclass_json
 
 from youtube_data_reader.models._identified_model_base import _IdentifiedModelBase
@@ -10,4 +8,4 @@ from youtube_data_reader.models.utils import defaulted_dataclass
 @dataclass_json
 @defaulted_dataclass
 class Caption(_IdentifiedModelBase):
-    snippet: Optional[CaptionSnippet]
+    snippet: CaptionSnippet
